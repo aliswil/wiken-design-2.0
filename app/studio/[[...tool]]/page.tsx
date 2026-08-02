3d2d21538fecc1dc050dcff77b754cf6848d67e0
Alis Wiken Wilson <aliswkn@gmail.com>
2026-08-02 19:30:00 +0200

    Fix embedded Sanity Studio deployment

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,14 @@
-'use client';
-
 import { NextStudio } from 'next-sanity/studio';
 import config from '@/sanity.config';
 
+export const dynamic = 'force-static';
+
+export { metadata, viewport } from 'next-sanity/studio';
+
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return (
+    <div className="fixed inset-0 z-[100] bg-white">
+      <NextStudio config={config} />
+    </div>
+  );
 }
